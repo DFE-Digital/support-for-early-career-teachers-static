f67091be94acaa8f944db6fe1c9879a0c0132b9a
matt <mattonfoot@gmail.com>
2024-04-19 09:59:50 +0100

    include useful links in footer, materials, support, and top level of each course

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,4 +3,5 @@ Dir.glob(File.join('./lib', '**', '*.rb')).sort.each { |f| require f }
 use_helper Nanoc::Helpers::Rendering
 use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Breadcrumbs
+use_helper Nanoc::Helpers::ChildParent
 use_helper Nanoc::Helpers::XMLSitemap
